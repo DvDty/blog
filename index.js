64d2e4f5805e1bd62f9a3c469dbd7bb133143c79
Pavel <DvDty@users.noreply.github.com>
2022-03-12 16:41:01 +0200

    Add converter in service container

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import {ServiceContainer} from './src/ServiceContainer.js'
+import Config from './src/Config.js'
+import * as fs from 'fs'
+import showdown from  'showdown'
 
-ServiceContainer.singleton('config', () => {test: 123})
+ServiceContainer.singleton('config', () => {
+    let configFile = fs.readFileSync('config.json', {encoding: 'utf8'})
+    return new Config(configFile)
+})
 
-console.log(ServiceContainer.get('config').test)
+ServiceContainer.singleton('converter', () => {
+    return new showdown.Converter()
+})
